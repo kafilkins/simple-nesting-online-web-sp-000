190f63c0cfbfcdf0af6f86610429b673e8be16b9
Keith Filkins <kafilkins@me.com>
2020-12-06 17:05:09 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -63,7 +63,7 @@ def adding_matz
 #   :known_for => "Ruby",
 #    :languages => ["LISP", "C"]
 # }
-
+  programmer_hash = [:yukihiro_matsumoto {:known_for => "Ruby", :languages => ["LISP", "C"]}]
 	programmer_hash =
  		{
         :grace_hopper => {
@@ -79,7 +79,6 @@ def adding_matz
           :languages => ["C"]
         }
      }
-
 
 end
 
